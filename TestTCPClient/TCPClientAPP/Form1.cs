3a690be278f1b8089bc75f9ac935f7167b7e0ae5
JustJal <jalinson5@gmail.com>
2020-07-06 12:25:15 -0400

    A Server-Client basic app I made using a tutorial that I found on google

---
+++ TestTCPClient/TCPClientAPP/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TCPClientAPP
+{
+    public partial class Form1 : Form
+    {
+        static private NetworkStream stream;
+        static private StreamWriter streamw;
+        static private StreamReader streamr;
+        static private TcpClient client = new TcpClient();
+        static private string nick = "unknown";
+
+        private delegate void DAddItem(String s);
+
+        private void AddItem(String s)
+        {
+            Chat.Items.Add(s);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+            Nick.Visible = false;
+            Connection.Visible = false;
+            Chat.Visible = true;
+            Message.Visible = true;
+            Enviar.Visible = true;
+            pictureBox1.Visible = false;
+
+            nick = Nick.Text;
+
+            Conectar();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            label2.Visible = true;
+            Nick.Visible = true;
+            Connection.Visible = true;
+            Chat.Visible = false;
+            Message.Visible = false;
+            Enviar.Visible = false;
+            pictureBox1.Visible = true;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Enviar_Click(object sender, EventArgs e)
+        {
+            streamw.WriteLine(Message.Text);
+            streamw.Flush();
+            Message.Clear();
+        }
+
+        void Listen()
+        {
+            while (client.Connected)
+            {
+                try
+                {
+                    this.Invoke(new DAddItem(AddItem), streamr.ReadLine());
+                }
+                catch
+                {
+                    MessageBox.Show("Unable to connect");
+                    Application.Exit();
+                }
+            }
+        }
+
+        void Conectar()
+        {
+            try
+            {
+                client.Connect("127.0.0.1", 8000);
+                if (client.Connected)
+                {
+                    Thread t = new Thread(Listen);
+
+                    stream = client.GetStream();
+                    streamw = new StreamWriter(stream);
+                    streamr = new StreamReader(stream);
+
+                    streamw.WriteLine(nick);
+                    streamw.Flush();
+
+                    t.Start();
+                }
+                else
+                {
+                    MessageBox.Show("Server it's not available");
+                    Application.Exit();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Server it's not available");
+                Application.Exit();
+            }
+        }
+
+        private void Nick_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Message_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ TestTCPClient/TestTCPClient/Servidor.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Threading;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+
+namespace Servidor
+{
+    class Servidor_Chat
+    {
+        private TcpListener server;
+        private TcpClient client = new TcpClient();
+        private IPEndPoint ipendpoint = new IPEndPoint(IPAddress.Any, 8000);
+        private List<Conexion> list = new List<Conexion>();
+
+        Conexion con;
+
+        private struct Conexion
+        {
+            public NetworkStream stream;
+            public StreamWriter streamw;
+            public StreamReader streamr;
+            public string nick;
+        }
+
+        public Servidor_Chat()
+        {
+            Inicio();
+        }
+
+        public void Inicio()
+        {
+            Console.WriteLine("Server it's now running");
+            server = new TcpListener(ipendpoint);
+            server.Start();
+
+            while (true)
+            {
+                client = server.AcceptTcpClient();
+
+                con = new Conexion();
+                con.stream = client.GetStream();
+                con.streamr = new StreamReader(con.stream);
+                con.streamw = new StreamWriter(con.stream);
+
+                con.nick = con.streamr.ReadLine();
+
+                list.Add(con);
+                Console.WriteLine(con.nick + " it's connected");
+
+                Thread t = new Thread(Escuchar_Conexion);
+
+                t.Start();
+            }
+        }
+
+        void Escuchar_Conexion()
+        {
+            Conexion hcon = con;
+
+            do
+            {
+                try
+                {
+                    string tmp = hcon.streamr.ReadLine();
+                    Console.WriteLine(hcon.nick + " : " + tmp);
+                    foreach (Conexion c in list)
+                    {
+                        try
+                        {
+                            c.streamw.WriteLine(hcon.nick + " : " + tmp);
+                            c.streamw.Flush();
+                        }
+                        catch
+                        {
+                        }
+                    }
+                }
+                catch
+                {
+                    list.Remove(hcon);
+                    Console.WriteLine(con.nick + "it's disconnected.");
+                    break;
+                }
+            } while (true);
+        }
+    }
+}
