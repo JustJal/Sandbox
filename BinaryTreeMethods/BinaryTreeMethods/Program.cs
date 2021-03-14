5a4f93e56e315544f92b9398cf78f911ab1a40c6
JustJal <jalinson5@gmail.com>
2021-03-14 09:24:34 -0400

    I Erased the swap method since it is not necessary. DeleteMethod still not working.

--- BinaryTreeMethods/BinaryTreeMethods/Program.cs
+++ BinaryTreeMethods/BinaryTreeMethods/Program.cs
@@ -7,10 +7,11 @@ namespace BinaryTreeMethods
     public class Node
     {
         public int Key;
-        public Node Left, Right;
-        static Node Root; // If we have a tree, then we have a Root
+        public Node Left {get; set;}
+        public Node Right { get; set; }
+        static Node Root { get; set; } // If we have a tree, then we have a Root
         //This is the constructor
-        public Node (int Key)
+        public Node(int Key)
         {
             this.Key = Key;
             Left = Right = null;
@@ -34,15 +35,14 @@ namespace BinaryTreeMethods
                 Root = new Node(Key);// Thiis creates a new Root if there isnt any
                 return;
             }
-                
+
             Queue<Node> ToCheck = new Queue<Node>(); // First time I use this, I think is really useful. Thanks Microsoft! 
             ToCheck.Enqueue(temp);
 
             // The same as PreOrder method applies here
-            while(ToCheck.Count != 0)
+            while (ToCheck.Count != 0)
             {
-                temp = ToCheck.Peek(); // This returns the last element that has been added to the Queue
-                ToCheck.Dequeue(); // So now we drop the Node that we are checking from the queue
+               temp = ToCheck.Dequeue(); // So now we drop the Node that we are checking from the queue and return it to temp.
 
                 if (temp.Left == null)
                 {
@@ -62,10 +62,19 @@ namespace BinaryTreeMethods
             }
 
         }
-        /*static void Delete_Node(Node Root, Node DeleteNode)
+        static void Delete_Node(Node Root, Node DeleteNode) //It goes where it has to go but doesnt make anything.
         {
             if (Root is null)return;
-            
+
+            if (DeleteNode.Key < Root.Key)
+            {
+                Delete_Node(Root.Left, DeleteNode); //We check the left one
+            }
+            else if (DeleteNode.Key > Root.Key)
+            {
+                Delete_Node(Root.Right, DeleteNode); // We check the right one
+            }
+
             if (Root.Key == DeleteNode.Key)
             {
                 //No child nodes
@@ -75,43 +84,26 @@ namespace BinaryTreeMethods
                 }
                 else if (Root.Left == null) //No left child root
                 {
-                    Swap(ref Root, ref Root.Right); // We swap the root we are looking to delete to a child 
-                    Root.Right = null;                   //and then we delete it
+                    Root = Root.Right; // We swap the root we are looking to delete to a child 
+                    Root.Right = null;//and then we delete it
 
                 }
                 else if (Root.Right == null)//No right child root
                 {
-                    Swap(ref Root, ref Root.Left);
+                    Root = Root.Left;
                     Root.Left = null;
                 }
-
                 else // It is a complete node
                 {
                     Node Min = FindMin(Root.Left);
-                    Root.Key = Min.Key;
+                    Root = Min;
                     Delete_Node(Root.Left, Min);
 
                 }
             }
-            if (DeleteNode.Key > Root.Key)
-            {
-                Delete_Node(Root.Left, DeleteNode); //We check the left one
-            }
-            else
-            {
-                Delete_Node(Root.Right, DeleteNode); // We check the right one
-            }
-
-        }*/ //This method isnt working.
-
-        //Swap Generic method
-        static void Swap<T>(ref T A, ref T B)
-        {
-            // I made a tuple swap because im lazy to create a Temporal variable xd
-            (A, B) = (B, A);
         }
 
-        /*static Node FindMin(Node Temp) // This Method search for the nearest left node 
+        static Node FindMin(Node Temp) // This Method search for the nearest left node 
         {
             Node ThisOne = Temp;
 
@@ -120,43 +112,48 @@ namespace BinaryTreeMethods
                 ThisOne = ThisOne.Left;
             }
             return ThisOne;
-        }*/ // Since this method is part of Delete_Node it is unreferenced
+        }
 
         //Invert Recursive Method
         static void InvertTree(Node Temp)
         {
             if (Temp is null) return;
 
-            Swap(ref Temp.Left, ref Temp.Right); // We swap each child node of each node
+            Node _temp = Temp.Right;
+            Temp.Right = Temp.Left;
+            Temp.Left = _temp;  // We swap each child node of each node
 
             //We take the node as parent and pass through swap
-            InvertTree(Temp.Left); 
+            InvertTree(Temp.Left);
             InvertTree(Temp.Right);
         }
 
         static void Main(string[] args) //first we have to create our BinaryTree where Root.Left.Left = Null
         {
             //Im not creating a new object because im in the same class and are static
-            Root = new Node(1);//--------------------------1
-            Root.Left = new Node(2);//------------------/-----\
-            Root.Left.Left = new Node(4);//------------2-------3
-            Root.Right = new Node(3);//---------------/--\---/--\
-            Root.Right.Right = new Node(7);//--------4---N--6---7
-            Root.Right.Left = new Node(6);// Where N = null
-            
+            Root = new Node(10);//------------------------10
+            Root.Left = new Node(8);//------------------/----\
+            Root.Left.Left = new Node(4);//------------8-----21
+            Root.Right = new Node(21);//-------------/--\---/--\
+            Root.Right.Right = new Node(30);//------4---N--15--30
+            Root.Right.Left = new Node(15);// Where N = null
+
             // So now we print it
             Console.WriteLine("\n PreOrder transversal before inserting:");
             PreOrder(Root);
-            InsertNode(Root, 5);
+
+            InsertNode(Root, 12);
             Console.WriteLine("\n PreOrder transversal after inserting:");
             PreOrder(Root);
-            Console.WriteLine("\n PreOrder transversal after inverting:");
+
             InvertTree(Root);
+            Console.WriteLine("\n PreOrder transversal after inverting:");
             PreOrder(Root);
 
-            /*Console.WriteLine("\n PreOrder Transversal after Deleting:");
-            Delete_Node(Root, Root.Right.Left);
-            PreOrder(Root);*/ //This Method isnt Working.
+            InvertTree(Root); //I invert it again because its easier to read
+            Delete_Node(Root, Root.Left.Right);
+            Console.WriteLine("\n PreOrder Transversal after Deleting:");
+            PreOrder(Root);
 
             Console.ReadKey();
         }
