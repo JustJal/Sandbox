aafbef9c2882b22d22aa9265df3f8d7572b718fe
JustJal <jalinson5@gmail.com>
2021-04-02 10:57:35 -0400

    - I optimized it deleting one method.

--- 4-Kyu Permutations Problem (CodeWars)/4-Kyu Permutations Problem (CodeWars)/Program.cs
+++ 4-Kyu Permutations Problem (CodeWars)/4-Kyu Permutations Problem (CodeWars)/Program.cs
@@ -1,54 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 namespace Permutations
 {
     public class Permutations
     {
         public static List<string> PermutationsList = new List<string>();
-        static int NumberOfStrings;
+        static int NumberOfStrings = 0;
+        static string Word { get; set; }
         public static List<string> SinglePermutations(string word)
         {
             int MaxPermutations = word.Length <= 2 ? word.Length : Factorial(word.Length) / word.Length;
             // The max permutations when N > 2 is N!/N (I learned about permutations before making this code)
-            PermutationsList.Add(word); // The word is a solution
+
+            if (!PermutationsList.Contains(word))
+                PermutationsList.Add(word); // The word is a solution
+
             Char[] WordArray = word.ToCharArray();
 
-            while (!ItIsFull(PermutationsList, MaxPermutations)) //while the list is not full of different solutions
+            for (int i = 0; i < word.Length - 1; i++)
             {
-                for (int i = 0; i < word.Length - 1; i++)
+                for (int j = 0; j < word.Length - 1 - i; j++)
                 {
-                    for (int j = 0; j < word.Length - 1 - i; j++)
+                    var temp = WordArray[j];
+                    WordArray[j] = WordArray[j + 1];
+                    WordArray[j + 1] = temp;
+                    Word = GetString(WordArray);
+                    if (!PermutationsList.Contains(Word))
                     {
-                        var temp = WordArray[j];
-                        WordArray[j] = WordArray[j + 1];
-                        WordArray[j + 1] = temp;
-                        PermutationsList.Add(GetString(WordArray));
+                        PermutationsList.Add(Word);
+                        NumberOfStrings++;
                     }
-                    if (ItIsFull(PermutationsList, MaxPermutations))
-                        break;
                 }
+                if (NumberOfStrings == MaxPermutations)
+                    break;
             }
+
             return PermutationsList;
-        }
-
-        /* this method distinct every string in the list, 
-           if the list have the same differents solutions of max permutations, then it finish*/
-        public static bool ItIsFull(List<string> List, int MaxPermutations)
-        {
-            var differents = List.Distinct().ToList();
-            NumberOfStrings = 0;
-
-            foreach (string a in differents)
-            {
-                NumberOfStrings++;
-            }
-            if (NumberOfStrings == MaxPermutations)
-            {
-                PermutationsList = PermutationsList.Distinct().ToList(); //The same method makes the main list a list with different solutions
-                return true;
-            }
-            return false;
         }
         private static int Factorial(int n, int Returner = 1)
         {
@@ -57,13 +44,6 @@ namespace Permutations
 
             return Returner;
         }
-        public static void PrintList(List<string> List)
-        {
-            foreach (string a in List)
-            {
-                Console.WriteLine(a);
-            }
-        }
 
         private static string GetString(char[] array)
         {
@@ -71,7 +51,15 @@ namespace Permutations
 
             return str;
         }
+        public static void PrintList(List<string> List)
+        {
+            foreach (string a in List)
+            {
+                Console.WriteLine(a);
+            }
+        }
     }
+
     class Program
     {
         static void Main(string[] args)
@@ -82,3 +70,4 @@ namespace Permutations
         }
     }
 }
+
