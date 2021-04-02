f1a53a36049a5d55cf22764d907cc182bc170954
JustJal <jalinson5@gmail.com>
2021-04-02 14:45:49 -0400

    - My solution for that problem in Code Wars (fails the random tests, I dont know why)

---
+++ 6to-Kyu Sum of Parts/6to-Kyu Sum of Parts/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace SumOfParts
+{
+    class SumParts
+    {
+        public static int[] PartsSums(int[] ls)
+        {
+            int End = ls.Length;
+            int[] Solution = new int[End+1];
+            int Counter = 0;
+
+                do
+                {
+                    Solution[Counter] = ls.Sum();
+                    ls = ls.Where(x => x != ls[0])
+                           .ToArray();
+                    Counter++;
+                } while (ls.Length != 0);
+
+            return Solution;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] xd = new int[6] { 1, 2, 3, 4, 5, 6 };
+           xd = SumParts.PartsSums(xd);
+            foreach (int a in xd)
+                Console.Write(a+" ");
+
+            Console.ReadKey();
+        }
+    }
+}
